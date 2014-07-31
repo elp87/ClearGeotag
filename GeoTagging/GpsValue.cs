0794dec958fd2eac13d71073d06e4ca4d9e15c96
elp87 <elp1987@gmail.com>
2014-07-31 18:44:41 +0400

    Get gps info

---
+++ GeoTagging/GpsValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+
+namespace GeoTagging
+{
+    public class GpsValue
+    {
+        private float _degree, _minutes, _seconds;
+
+        public GpsValue(PropertyItem ItemRef, PropertyItem Item)
+        {
+            this.CalcValue(ItemRef, Item);
+        }
+
+        public float Degree { get { return this._degree; } }
+        public float Minutes { get { return this._minutes; } }
+        public float Seconds { get { return this._seconds; } }
+
+        private void CalcValue(PropertyItem ItemRef, PropertyItem Item)
+        {
+            UInt32 latitudeDegreeNumerator = BitConverter.ToUInt32(Item.Value, 0);
+            UInt32 latitudeDegreeDenominator = BitConverter.ToUInt32(Item.Value, 4);
+            this._degree = (float)latitudeDegreeNumerator / (float)latitudeDegreeDenominator;
+
+            UInt32 latitudeMinutesNumerator = BitConverter.ToUInt32(Item.Value, 8);
+            UInt32 latitudeMinutesDenominator = BitConverter.ToUInt32(Item.Value, 12);
+            this._minutes = (float)latitudeMinutesNumerator / (float)latitudeMinutesDenominator;
+
+            UInt32 latitudeSecondsNumerator = BitConverter.ToUInt32(Item.Value, 16);
+            UInt32 latitudeSecondsDenominator = BitConverter.ToUInt32(Item.Value, 20);
+            this._seconds = (float)latitudeSecondsNumerator / (float)latitudeSecondsDenominator;
+        }
+
+        public override string ToString()
+        {
+            return this._degree.ToString() + @"° " + this._minutes.ToString() + @"' " + this._seconds.ToString() + "\"";
+        }
+    }
+}
---
+++ GeoTagging/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace GeoTagging
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = ".jpg";
+            ofd.Filter = "Jpg Files (*.jpg; *.jpeg)|*.jpg; *.jpeg";
+            if (ofd.ShowDialog() == true)
+            {
+                string filename = ofd.FileName;
+                LoadedImage.Source = new BitmapImage(new Uri(filename, UriKind.Absolute));
+
+                Image image = new Bitmap(filename);
+
+                PropertyItem piLatitudeRef = image.GetPropertyItem(1);
+                PropertyItem piLatitude = image.GetPropertyItem(2);
+                PropertyItem piLongtitudeRef = image.GetPropertyItem(3);
+                PropertyItem piLongtitude = image.GetPropertyItem(4);
+                PropertyItem piAltitudeRef = image.GetPropertyItem(5);
+                PropertyItem piAltitude = image.GetPropertyItem(6);
+
+                GpsValue latitude = new GpsValue(piLatitudeRef, piLatitude);
+                GpsValue longtitude = new GpsValue(piLongtitudeRef, piLongtitude);
+                InfoLabel.Content = "Latitude - " + latitude.ToString() + "\n";
+                InfoLabel.Content += "Longtitude - " + longtitude.ToString() + "\n";
+
+
+
+                UInt32 altN = BitConverter.ToUInt32(piAltitude.Value, 0);
+                UInt32 altD = BitConverter.ToUInt32(piAltitude.Value, 4);
+                float alt = (float)altN / (float)altD;
+                InfoLabel.Content += "Altitude - " + alt.ToString();
+
+            }
+        }
+    }
+}
