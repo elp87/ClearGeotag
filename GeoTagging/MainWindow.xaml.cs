59671b8cccbfe386f0ddd73965c72f3d3d76efbe
elp87 <elp1987@gmail.com>
2014-08-07 18:21:44 +0400

    it works

--- GeoTagging/MainWindow.xaml.cs
+++ GeoTagging/MainWindow.xaml.cs
@@ -27,77 +27,58 @@ namespace GeoTagging
             if (ofd.ShowDialog() == true)
             {
                 string filename = ofd.FileName;
-                //LoadedImage.Source = new BitmapImage(new Uri(filename, UriKind.Absolute));
 
-                FileStream photoStream = File.Open(
+                BitmapDecoder decoder = null;
+                BitmapFrame frame = null;
+                BitmapMetadata metadata = null;
+
+                using (FileStream photoStream = File.Open(
                     filename,
-                    FileMode.OpenOrCreate,
-                    FileAccess.ReadWrite
-                    );
-
-
-
-                BitmapDecoder decoder = JpegBitmapDecoder.Create(
+                    FileMode.Open,
+                    FileAccess.ReadWrite,
+                    FileShare.None
+                    ))
+                {
+                    decoder = JpegBitmapDecoder.Create(
                     photoStream,
-                    BitmapCreateOptions.IgnoreColorProfile,
-                    BitmapCacheOption.Default
+                    BitmapCreateOptions.PreservePixelFormat,
+                    BitmapCacheOption.OnLoad
                     );
-                BitmapSource frame = decoder.Frames[0].Clone();
-                var thumbnail = decoder.Frames[0].Thumbnail.Clone();
-                var colorContexts = decoder.Frames[0].ColorContexts;
-                BitmapMetadata exif = (BitmapMetadata)frame.Metadata.Clone();
+                }
 
-                
+                frame = decoder.Frames[0];
+                metadata = (BitmapMetadata)frame.Metadata;
 
-                exif.RemoveQuery("/app1/ifd/gps/{ushort=1}");
-                exif.RemoveQuery("/app1/ifd/gps/{ushort=2}");
-                exif.RemoveQuery("/app1/ifd/gps/{ushort=3}");
-                exif.RemoveQuery("/app1/ifd/gps/{ushort=4}");
-                exif.RemoveQuery("/app1/ifd/gps/{ushort=5}");
-                exif.RemoveQuery("/app1/ifd/gps/{ushort=6}");
-                //photoStream.Close();
+                if (metadata != null)
+                {
+                    BitmapMetadata newMetaData = (BitmapMetadata)frame.Metadata.Clone();
 
-                JpegBitmapEncoder encoder = new JpegBitmapEncoder();
-                encoder.Frames.Add(
-                    BitmapFrame.Create(
+                    newMetaData.RemoveQuery("/app1/ifd/gps/{ushort=1}");
+                    newMetaData.RemoveQuery("/app1/ifd/gps/{ushort=2}");
+                    newMetaData.RemoveQuery("/app1/ifd/gps/{ushort=3}");
+                    newMetaData.RemoveQuery("/app1/ifd/gps/{ushort=4}");
+                    newMetaData.RemoveQuery("/app1/ifd/gps/{ushort=5}");
+                    newMetaData.RemoveQuery("/app1/ifd/gps/{ushort=6}");
+
+                    JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(
                         frame,
-                        thumbnail,
-                        exif,
-                        colorContexts
-                        )
-                    );
-                //photoStream.Close();
-                encoder.Save(photoStream);
+                        frame.Thumbnail,
+                        newMetaData,
+                        frame.ColorContexts
+                        ));
 
+                    File.Delete(filename);
 
-                /*object data = exif.GetQuery("/app1/ifd/gps/{ushort=1}");
-
-                string dataValue = data.ToString();
-                InfoLabel.Content = dataValue;*/
-
-                /*Image image = new Bitmap(filename);
-
-                PropertyItem piLatitudeRef = image.GetPropertyItem(1);
-                PropertyItem piLatitude = image.GetPropertyItem(2);
-                PropertyItem piLongtitudeRef = image.GetPropertyItem(3);
-                PropertyItem piLongtitude = image.GetPropertyItem(4);
-                PropertyItem piAltitudeRef = image.GetPropertyItem(5);
-                PropertyItem piAltitude = image.GetPropertyItem(6);
-
-                GpsValue latitude = new GpsValue(piLatitudeRef, piLatitude);
-                GpsValue longtitude = new GpsValue(piLongtitudeRef, piLongtitude);
-                InfoLabel.Content = "Latitude - " + latitude.ToString() + "\n";
-                InfoLabel.Content += "Longtitude - " + longtitude.ToString() + "\n";
-
-
-
-                UInt32 altN = BitConverter.ToUInt32(piAltitude.Value, 0);
-                UInt32 altD = BitConverter.ToUInt32(piAltitude.Value, 4);
-                float alt = (float)altN / (float)altD;
-                InfoLabel.Content += "Altitude - " + alt.ToString();*/
-
-
-
+                    using (Stream photoStream = File.Open(
+                        filename,
+                        FileMode.CreateNew,
+                        FileAccess.ReadWrite
+                        ))
+                    {
+                        encoder.Save(photoStream);
+                    }
+                }
             }
         }
     }
